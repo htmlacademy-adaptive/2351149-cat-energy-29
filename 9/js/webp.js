4cd140478913c7e504b70490afad26d45167f538
Keks <keks@htmlacademy.ru>
2023-09-06 16:19:34 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/webp.js
+++ 9/js/webp.js
@@ -5,9 +5,9 @@ function checkWebP(callback) {
       callback(webP.height === 2);
     };
 
-    webp.addEventListener("load", event);
+    webP.addEventListener("load", event);
 
-    webp.addEventListener("error", event);
+    webP.addEventListener("error", event);
 
     webP.src =
       "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
