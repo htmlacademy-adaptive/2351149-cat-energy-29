97611b9948c6c8338eb7befb81944798e09162cf
Jar <dev.bazylev@gmail.com>
2023-09-08 20:32:19 +0300

    second try

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -2,6 +2,8 @@ let nav = document.querySelector('.header__navigation');
 let burger = document.querySelector('.header__toggle');
 
 nav.classList.remove('header__navigation--nojs');
+nav.classList.toggle('header__navigation--closed'),
+nav.classList.toggle('header__navigation--opened');
 
 burger.onclick = function() {
   nav.classList.toggle('header__navigation--closed'),
